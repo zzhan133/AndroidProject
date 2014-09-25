93de67e6c10a283bb08b3ae40179bbcd4dd4e82c
Zhe Zhang <tmzhangzhe@gmail.com>
2014-09-24 21:53:35 -0700

    an available activity

--- MyFirstApp/src/com/example/myfirstapp/DisplayMessageActivity.java
+++ MyFirstApp/src/com/example/myfirstapp/DisplayMessageActivity.java
@@ -1,11 +1,16 @@
 package com.example.myfirstapp;
 
+
+//import course.examples.Threading.ThreadingAsyncTask.R;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.graphics.PorterDuff;
 
 public class DisplayMessageActivity extends Activity {
 
@@ -13,13 +18,56 @@ public class DisplayMessageActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_display_message);
+		
+		
+		//Zhe Zhang add button showing
+				final Button buttonOne = (Button) findViewById(R.id.slotOne);
+				buttonOne.getBackground().setColorFilter(0xFFFF0000, PorterDuff.Mode.MULTIPLY);
+//				final Button buttonTwo = (Button) findViewById(R.id.slotTwo);
+//				buttonTwo.getBackground().setColorFilter(0xFFFF0000, PorterDuff.Mode.MULTIPLY);
+				final Button buttonThree = (Button) findViewById(R.id.slotThree);
+				buttonThree.getBackground().setColorFilter(0xFFFF0000, PorterDuff.Mode.MULTIPLY);
+//				final Button buttonFour = (Button) findViewById(R.id.slotFour);
+//				buttonFour.getBackground().setColorFilter(0xFFFF0000, PorterDuff.Mode.MULTIPLY);
+				final Button buttonFive = (Button) findViewById(R.id.slotFive);
+				buttonFive.getBackground().setColorFilter(0xFFFF0000, PorterDuff.Mode.MULTIPLY);
+//				final Button buttonSix = (Button) findViewById(R.id.slotSix);
+//				buttonSix.getBackground().setColorFilter(0xFFFF0000, PorterDuff.Mode.MULTIPLY);
+		
+		
 		Intent intent = getIntent();
 		String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-		TextView textView = new TextView(this);
-		textView.setTextSize(40);
-		textView.setText(message);
+		
+		int num = Integer.parseInt(message);
+		
+		Button tmp = buttonOne;
+		switch(num){
+		case 1: tmp = buttonOne;
+			break;
+//		case 2: tmp = buttonTwo;
+//			break;
+		case 3: tmp = buttonThree;
+			break;
+//		case 4: tmp = buttonFour;
+//			break;
+		case 5: tmp = buttonFive;
+			break;
+//		case 6: tmp = buttonSix;
+//			break;
+		default:
+			break;
+		}
+		
+		tmp.getBackground().setColorFilter(0xFF00FF00, PorterDuff.Mode.MULTIPLY);
+		
+//		TextView textView = new TextView(this);
+//		textView.setTextSize(40);
+//		textView.setText(message);
 		//Set the text view as the activity layout
-		setContentView(textView);
+//		setContentView(textView);
+		
+		
+		
 	}
 
 	@Override
@@ -48,6 +96,10 @@ public class DisplayMessageActivity extends Activity {
 
 	private void openSearch() {
 		// TODO Auto-generated method stub
+		
+	}
+	
+	public void sendMessage(View view){
 		
 	}
 }
