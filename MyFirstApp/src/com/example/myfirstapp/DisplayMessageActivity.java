f49ea78da9dddd90b5e9c17c824502e9206d76fd
Zhe Zhang <tmzhangzhe@gmail.com>
2014-10-02 21:46:52 -0700

    Add the function to show the price, but the gui part need to change.

--- MyFirstApp/src/com/example/myfirstapp/DisplayMessageActivity.java
+++ MyFirstApp/src/com/example/myfirstapp/DisplayMessageActivity.java
@@ -9,7 +9,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.graphics.PorterDuff;
 
 public class DisplayMessageActivity extends Activity {
--- MyFirstApp/src/com/example/myfirstapp/MainActivity.java
+++ MyFirstApp/src/com/example/myfirstapp/MainActivity.java
@@ -1,16 +1,14 @@
 package com.example.myfirstapp;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Gravity;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
@@ -23,25 +21,43 @@ public class MainActivity extends Activity {
 	
 	//false represent empty; true represents occupied
 	private static Boolean colorStatus[] = {false, false, false, false, false, false, false, false, false, false};
+	
+	//记录停车开始时间
 	private long startTime[] = {0,0,0,0,0,0,0,0,0,0};
 	
 	private static int resourceId[] = {R.id.slotOne, R.id.slotTwo,
 			R.id.slotThree,R.id.slotFour,R.id.slotFive,R.id.slotSix,R.id.slotSeven,R.id.slotEight,R.id.slotNine,R.id.slotTen};
 	
+	private static int priceResourceId[] = {R.id.priceOne, R.id.priceTwo, R.id.priceThree,R.id.priceFour,R.id.priceFive
+				,R.id.priceSix,R.id.priceSeven,R.id.priceEight,R.id.priceNine,R.id.priceTen};
+	
+	
+	private TextView[] textPriceView;
+	
 	private TextView textView;
 	
 //	private Calendar parkingTime[10] = {02};
+	
+	//add price area by zz 2014年10月25:17	
+	private Handler priceShowingHandler = new Handler();
+	//end by zz 2014年10月2日16:25:53
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		textView = (TextView)findViewById(R.id.remainingSlots);
+		
 		buttonArray = new Button[10];
+		textPriceView = new TextView[10];
 		
 		for(int i = 0; i < 10; i++){
 			buttonArray[i] = (Button) findViewById(resourceId[i]);
+			textPriceView[i] = (TextView) findViewById(priceResourceId[i]);
 		}
+		
 		
 		showButtonColor();
 	}
@@ -59,14 +75,77 @@ public class MainActivity extends Activity {
 		}
 		
 		//Context context = new Context();
-		textView = (TextView)findViewById(R.id.remainingSlots);
+		
 		String str = Integer.toString(remaining);
 		textView.setTextSize(40);
 		//Set the text view as the activity layout
 		textView.setText(str);
 		
+		priceShowingHandler.postDelayed(updatePrice, 0);
+		
 	}
 
+
+	
+	/**Called when the user clicks the Send button */
+	public void sendMessage(View view){
+		//Do something in response to button
+		//Intent intent = new Intent(this, DisplayMessageActivity.class);
+		EditText editText = (EditText)findViewById(R.id.edit_message);
+		String message = editText.getText().toString();
+		//intent.putExtra(EXTRA_MESSAGE, message);
+		//startActivity(intent);
+		
+		int num = Integer.parseInt(message);
+		if(!(num > 0 && num < 10)){
+			return;
+		}
+		if(colorStatus[num-1])
+		{
+			colorStatus[num-1] = false;
+			startTime[num-1] = 0;
+		}
+		else{
+			colorStatus[num-1] = true;
+			startTime[num-1] = SystemClock.uptimeMillis();
+		}
+			
+		showButtonColor();
+		
+	}
+	
+	public void showTime(View view){
+		Button tmpButton = (Button) view;
+		
+		tmpButton.getBackground().setColorFilter(0xFFFF0000, PorterDuff.Mode.MULTIPLY);		
+	}
+	
+	
+	private void showPrice(){
+		for(int i = 0; i < 10; i++){
+			if(colorStatus[i]){
+				long timeInMilliseconds = SystemClock.uptimeMillis() - startTime[i];
+				int secs = (int) (timeInMilliseconds / 1000);
+				int mins = secs / 60;
+				String str = Integer.toString(mins);
+				textPriceView[i].setTextSize(40);
+				//Set the text view as the activity layout
+				textPriceView[i].setText(str);
+			}
+		}
+	}
+	
+	
+	private Runnable updatePrice = new Runnable() {
+
+		public void run() {
+			showPrice();
+			priceShowingHandler.postDelayed(this, 1000);
+		}
+
+	};
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -84,34 +163,5 @@ public class MainActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
-	}
-	
-	/**Called when the user clicks the Send button */
-	public void sendMessage(View view){
-		//Do something in response to button
-		//Intent intent = new Intent(this, DisplayMessageActivity.class);
-		EditText editText = (EditText)findViewById(R.id.edit_message);
-		String message = editText.getText().toString();
-		//intent.putExtra(EXTRA_MESSAGE, message);
-		//startActivity(intent);
-		
-		int num = Integer.parseInt(message);
-		if(colorStatus[num-1])
-		{
-			colorStatus[num-1] = false;
-			startTime[num-1] = 0;
-		}
-		else{
-			colorStatus[num-1] = true;
-			startTime[num-1] = System.currentTimeMillis();;
-		}
-			
-		showButtonColor();
-		
-	}
-	
-	public void changeColor(View view){
-		Button tmpButton = (Button) view;
-		tmpButton.getBackground().setColorFilter(0xFFFF0000, PorterDuff.Mode.MULTIPLY);		
 	}
 }
